50eeec596e2b100eb431b1e4135dce76cabce512
Guillermo Pastor <sparta015@gmail.com>
2017-03-13 16:22:39 +0100

    First stage

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,37 @@
 import * as dotenv from 'dotenv';
 import * as path from 'path';
+import * as restify from 'restify';
+import * as botbuilder from 'botbuilder';
+import plugin from './plugins/greeting/index'
 
 // Load configuration in process.env from the .env file
 dotenv.config();
 
-import { Bot, BotBuilder, BotServerRunner, BotConsoleRunner, Startup } from '@telefonica/bot-core';
-
-// Create the bot
-let bot = new Bot({
-    // The luis models to be consulted
-    modelMapSet: [
-        { 'en-us': process.env.LUIS_MODEL__CORTANA__EN_US }
-    ],
-    // The plugins to use
-    plugins: [
-        path.join(__dirname, 'plugins', 'basic')
-    ],
-    // Language settings
-    localizerSettings: {
-        botLocalePath: path.join(__dirname, '..', 'locale'),
-        defaultLocale: process.env.BOT_DEFAULT_LOCALE || 'en-us'
-    }
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
-console.log(bot);
-// Lets go to startup our bot
-let startup = new Startup();
+let connector = new botbuilder.ChatConnector({
+    appId: process.env.BOT_APP_ID,
+    appPassword: process.env.BOT_APP_PASSWD
+});
+server.post('/api/messages', connector.listen());
 
-if (process.env.NODE_ENV === 'development') {
-    // During development, we interact with the bot using our terminal
-    let runner = new BotConsoleRunner({
-        bot
-    });
-    startup.use(runner);
-} else {
-    // but in production, we publish a WebServer with the Micorosoft credentials
-    // so it can be accessed by the MS BotFramework (MS Gateway)
-    let runner = new BotServerRunner({
-        bot,
-        port: parseInt(process.env.BOT_PORT, 10) || 8080,
-        appId: process.env.MICROSOFT_APP_ID,
-        appPassword: process.env.MICROSOFT_APP_PASSWORD
-    });
-    startup.use(runner);
-}
+let bot = new botbuilder.UniversalBot(connector, (session: botbuilder.Session) => {
+    session.endDialog("Sorry, I did not understand you");
+});
 
-// All done. Launch!
-startup.bootstrap('bot');
+const model = process.env.LUIS_MODEL;
+
+bot.recognizer(new botbuilder.LuisRecognizer(model));
+
+bot.library(plugin);
+
+bot.dialog('help', [
+    (session) => {
+        session.endDialog('Need help?, try asking me something like: What is the value of Awesome strock? and I give you the value');
+    }
+]).triggerAction({
+    matches: 'picnic.intent.help'
+});
--- src/plugins/greeting/index.ts
+++ src/plugins/greeting/index.ts
@@ -2,14 +2,15 @@
  * A plugin just exports a "library" whose dialogs are the LUIS intents the plugin can understand
  */
 
-import { BotBuilder } from '@telefonica/bot-core';
-
-import helloworld from './picnic.intent.greeting';
+import * as BotBuilder from 'botbuilder';
 
 // create a plugin, the id must be unique in your bot
-let plugin = new BotBuilder.Library('helloworld');
+let plugin = new BotBuilder.Library('greeting');
+
 // Add the dialogs, one by one, that this plugin can manage
 // The dialog name (1st param) is the intent name as defined in LUIS
-plugin.dialog('picnic.intent.greeting', helloworld);
+plugin.dialog('greeting', BotBuilder.DialogAction.endDialog('hello World')).triggerAction({
+    matches: 'picnic.intent.greeting'
+});
 
 export default plugin;
