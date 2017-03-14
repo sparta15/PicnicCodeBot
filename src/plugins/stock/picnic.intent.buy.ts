3516ad28461ff7993f691020c5f3c2f460a9bb57
Guillermo Pastor <sparta015@gmail.com>
2017-03-14 21:08:53 +0100

    prompts

--- src/plugins/stock/picnic.intent.buy.ts
+++ src/plugins/stock/picnic.intent.buy.ts
@@ -12,24 +12,25 @@ export default [
 
 function numberStock(session: BotBuilder.Session, args:any, next: Function) {
     BotBuilder.Prompts.number(session, 'How many stocks do you want to buy?');
-    next();
 }
 
 function costStock(session:BotBuilder.Session, args: any, next: Function) {
     if(!args.response) {
         return session.endDialog('Okay I don\'t buy it');
     }
-    session.dialogData.number = args.response;
-    let amount = cost(session.dialogData.number);
-    BotBuilder.Prompts.confirm(session, `The cost of the operation will be ${amount}, Are you sure ?`);
-    next();
+    if(isNaN(args.response)) {
+        return session.endDialog('This is not a valid number');
+    }
+    session.dialogData.number = args.respone;
+    value(args.response, session);
 }
 
+
 async function buyStock(session: BotBuilder.Session, args: any) {
-    if(args.response === 'no'){
-        return session.endDialog('Sure, I don\'t d it');
+    if(!args.response) {
+        return session.endDialog('Sure, I don\'t do it');
     }
-    session.send(`Okey, I\'m going to buy ${session.dialogData.number} of it`);
+    session.send(`Okey, I\'m going to buy it`);
     await sleep(3000);
     session.endDialog('Great, you have new stocks');
 }
@@ -38,15 +39,16 @@ function sleep (time: any) {
     return new Promise (resolve => setTimeout(resolve, time));
 }
 
-function cost(stocks : number): number {
-    let cost : number = 0;
-    fs.readFile('quote.csv', (err, data) => {
+function value(number: number, session: BotBuilder.Session): Promise<void> {
+    return new Promise<void> (() => {
+        fs.readFile('quote.csv', (err, data) => {
             if (err) {
                 console.log('ERROR: ', err);
             } else {
-                let value = _.split(data.toString(), ',')[4];
-                cost = Number(value) * stocks;
+                let value = Number(_.split(data.toString(), ',')[4]);
+                let cost = number * value;
+                BotBuilder.Prompts.confirm(session, `The cost of the operation will be ${cost}, Are you sure ?`);
             }
-    })
-    return cost;
+        });
+    });
 }
--- src/plugins/stock/picnic.intent.stock.ts
+++ src/plugins/stock/picnic.intent.stock.ts
@@ -21,20 +21,21 @@ export default [
 function mapStock(session: BotBuilder.Session, args: any, next:Function) {
     let stockName = BotBuilder.EntityRecognizer.findEntity(args.intent.entities, 'company');
     let upper = _.toUpper(stockName.entity);
-
+    session.dialogData.company = upper;
     let stockacronym = dictionary.filter((item) => {
         return item.key === upper;
     });
     if (stockacronym.length === 0) {
         return session.endDialog('I don\'t know about this company');
     }
-    session.dialogData.company = stockacronym[0].value;
+    session.dialogData.acronym = stockacronym[0].value;
     next();
 };
 
 function priceStock(session: BotBuilder.Session, args: any, next: Function) {
-    let company = session.dialogData.company;
-    let stream = request.get(`http://finance.yahoo.com/d/quotes.csv?s=${company}&f=sb2b3jk`)
+    let company = session.dialogData.company
+    let acronym = session.dialogData.acronym;
+    let stream = request.get(`http://finance.yahoo.com/d/quotes.csv?s=${acronym}&f=sb2b3jk`)
                         .pipe(fs.createWriteStream('quote.csv'));
 
     stream.on('finish', () => {
@@ -46,9 +47,9 @@ function priceStock(session: BotBuilder.Session, args: any, next: Function) {
                 session.send(`The value of ${company} is : ${value}`);
                 let card = new BotBuilder.HeroCard(session)
                             .title(`${company} stocks`)
-                            .images([BotBuilder.CardImage.create(session, `https://chart.finance.yahoo.com/z?s=${company}&t=6m&q=l&l=on&z=s&p=m50,m100`)])
+                            .images([BotBuilder.CardImage.create(session, `https://chart.finance.yahoo.com/z?s=${acronym}&t=6m&q=l&l=on&z=s&p=m50,m100`)])
                             .buttons([
-                                BotBuilder.CardAction.imBack(session, `To buy stocks to ${company}`, 'Buy')
+                                BotBuilder.CardAction.imBack(session, `Buy stocks to ${company}`, 'Buy')
                             ]);
                 let msg = new BotBuilder.Message(session).attachments([card]);
                 session.endDialog(msg);
