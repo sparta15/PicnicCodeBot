d1f064b0f04b6a6e0ad772a199048e87f63253b2
Jorge Sanz <jorsanzp@gmail.com>
2017-03-16 15:10:40 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+export BOT_NAME="bot"
+export BOT_PORT="8080"
+export BOT_DEFAULT_LOCALE="en"
+export LUIS_MODEL__STOCK__EN="https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/e77b2908-9380-4d84-9377-f73c995b388e?subscription-key=08f5796ea4604e0c9921f5478f8e0962&verbose=true" 
+export MICROSOFT_APP_ID="" 
+export MICROSOFT_APP_PASSWORD=""
+export ENABLE_AUDIO_OUTPUT="false"
+npm run dev
